5c00a7105743ede80dfc9c5ace7dfb669230490d
ddongguri-bing <ckdalssnsk@naver.com>
2026-02-09 22:26:46 +0900

    H-Index

---
+++ H-Index.js
@@ -0,0 +1,13 @@
+function solution(citations) {
+  citations.sort((a, b) => b - a);
+
+  let answer = 0;
+
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] >= i + 1) answer = i + 1;
+  }
+
+  return answer;
+}
+
+console.log(solution([3, 0, 6, 1, 5])); // 3
